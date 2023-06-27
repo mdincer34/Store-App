a7e2e02f2a99325a43f9d61eaa7e178047153150
Mustafa <mustafadncr34@gmail.com>
2023-06-27 15:54:44 +0300

    Basics done i'll do design layout

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers;
+
+public class ProductController : Controller
+{
+    // dependency injection
+    private readonly RepositoryContext _context;
+
+    public ProductController(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+
+    public IActionResult Index()
+    {
+        var model = _context.Products.ToList();
+        return View(model);
+    }
+
+    public IActionResult Get(int id)
+    {
+        Product product = _context.Products.FirstOrDefault(p => p.ProductId.Equals(id));
+        return View(product);
+    }
+}
